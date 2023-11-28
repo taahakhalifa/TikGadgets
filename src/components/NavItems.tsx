2fce8cebf3dd81ff03c3775e598661d18539e259
Taaha Khalifa <taahakhalifa@outlook.com>
2023-11-28 18:54:29 +0000

    click event handlers added to navbar so that when user clicks away from navbar, the dropdown goes alongside it. likewise when user clicks on the escape key, the navbar dropdown disappears also

--- src/components/NavItems.tsx
+++ src/components/NavItems.tsx
@@ -1,19 +1,35 @@
 "use client";
-import { useRef, useState } from "react";
+import { useEffect, useRef, useState } from "react";
 import { PRODUCT_CATEGORIES } from "../config";
+import { useOnClickOutside } from "../hooks/use-on-click-outside";
 import NavItem from "./NavItem";
 
 const NavItems = () => {
     const [activeIndex, setActiveIndex] = useState<null | number>(null);
 
-    const isAnyOpen = activeIndex !== null
+    useEffect(() => {
+        const handler = (e: KeyboardEvent) => {
+            if (e.key === "Escape") {
+                setActiveIndex(null);
+            }
+        };
 
-    const navRef = useRef<HTMLDivElement | null>(null)
+        document.addEventListener("keydown", handler);
+
+        return () => {
+          document.removeEventListener("keydown", handler)
+        }
+    }, []);
+
+    const isAnyOpen = activeIndex !== null;
+
+    const navRef = useRef<HTMLDivElement | null>(null);
+
+    useOnClickOutside(navRef, () => setActiveIndex(null));
 
     return (
-        <div className="flex gap-4 h-full">
+        <div className="flex gap-4 h-full" ref={navRef}>
             {PRODUCT_CATEGORIES.map((category, i) => {
-
                 const handleOpen = () => {
                     if (activeIndex === i) {
                         setActiveIndex(null);
@@ -22,11 +38,17 @@ const NavItems = () => {
                     }
                 };
 
-                const isOpen = i === activeIndex
+                const isOpen = i === activeIndex;
 
                 return (
-                  <NavItem category={category} handleOpen={handleOpen} isOpen={isOpen} key={category.value} isAnyOpen={isAnyOpen}/>
-                )
+                    <NavItem
+                        category={category}
+                        handleOpen={handleOpen}
+                        isOpen={isOpen}
+                        key={category.value}
+                        isAnyOpen={isAnyOpen}
+                    />
+                );
             })}
         </div>
     );
