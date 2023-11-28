3d93a4a1909271fb95ac9a50d8d96a711c5de4ae
Taaha Khalifa <taahakhalifa@outlook.com>
2023-11-28 13:30:04 +0000

    p tag under h1 tag added alongside a Link which would direct individuals to a products page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,24 @@
-import MaxWidthWrapper from "../components/MaxWidthWrapper"
+import Link from "next/link";
+import MaxWidthWrapper from "../components/MaxWidthWrapper";
 
 export default function Home() {
-  return (<MaxWidthWrapper>
-    <div className='py-20 mx-auto text-center flex flex-col items-center max-w-3xl'>
-      <h1 className="text-4xl font-bold tracking-tight">Uncover TikTok's Hidden Gems: Must-Have <span className='text-blue-600'>Products</span> and <span className='text-blue-600'>Gadgets</span>.</h1>
-    </div>
-  </MaxWidthWrapper>
-  )
+    return (
+        <MaxWidthWrapper>
+            <div className="py-20 mx-auto text-center flex flex-col items-center max-w-3xl">
+                <h1 className="text-4xl font-bold tracking-tight">
+                    Uncover TikTok's Hidden Gems: Must-Have{" "}
+                    <span className="text-blue-600">Products</span> and{" "}
+                    <span className="text-blue-600">Gadgets</span>.
+                </h1>
+                <p className="mt-6 text-lg max-w-prose text-muted-foreground">
+                    Welcome to the world of TikTok's hidden gems, where you'll
+                    discover must-have products and gadgets that bring joy to
+                    your life. Let's explore together!
+                </p>
+                <div className="flex flex-col sm:flex-row gap-4 mt-6">
+                    <Link href="/products">Browse Trending</Link>
+                </div>
+            </div>
+        </MaxWidthWrapper>
+    );
 }
