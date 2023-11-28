b1447ec37909737f234f62cd0f914c81fd4ddea1
Taaha Khalifa <taahakhalifa@outlook.com>
2023-11-28 16:59:02 +0000

    general navbar added

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,7 @@
 import { cn } from "@/lib/utils";
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
+import Navbar from "../components/Navbar";
 import "./globals.css";
 
 const inter = Inter({ subsets: ["latin"] });
@@ -23,6 +24,7 @@ export default function RootLayout({
                     inter.className
                 )}
             >
+                <Navbar />
                 <main className="relative flex flex-col min-h-screen">
                     <div className="flex-grow flex-1"> {children}</div>
                 </main>
