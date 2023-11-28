9851bb1f72b051c444ade3338cf06c39786d5624
Taaha Khalifa <taahakhalifa@outlook.com>
2023-11-28 13:23:45 +0000

    the main header of the site has been added, a h1 tag

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,10 @@
+import MaxWidthWrapper from "../components/MaxWidthWrapper"
+
 export default function Home() {
-  return (
-    <div>Hello world</div>
+  return (<MaxWidthWrapper>
+    <div className='py-20 mx-auto text-center flex flex-col items-center max-w-3xl'>
+      <h1 className="text-4xl font-bold tracking-tight">Uncover TikTok's Hidden Gems: Must-Have <span className='text-blue-600'>Products</span> and <span className='text-blue-600'>Gadgets</span>.</h1>
+    </div>
+  </MaxWidthWrapper>
   )
 }
