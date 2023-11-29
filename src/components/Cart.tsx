f44a0973fcbd6d69465e601574914dd065b659b2
Taaha Khalifa <taahakhalifa@outlook.com>
2023-11-29 16:24:05 +0000

    basic layout of the cart list has been added WITHOUT functionality

--- src/components/Cart.tsx
+++ src/components/Cart.tsx
@@ -1,4 +1,5 @@
 "ues client";
+import { Separator } from "@/components/ui/separator";
 import {
     Sheet,
     SheetContent,
@@ -9,6 +10,9 @@ import {
 import { ShoppingCart } from "lucide-react";
 
 const Cart = () => {
+
+const itemCount = 1
+
     return (
         <Sheet>
             <SheetTrigger className="group -m-2 flex items-center p-2">
@@ -24,6 +28,28 @@ const Cart = () => {
                 <SheetHeader className="space-y-2.5 pr-6">
                     <SheetTitle>Cart {0}</SheetTitle>
                 </SheetHeader>
+                {itemCount > 0 ? (
+                  <>
+                  <div className="flex w-full flex-col pr-6">
+                    {/* TODO : cart logic */}
+                    Cart Item</div>
+                    <div className="space-y-4 pr-6">
+                      <Separator />
+                      <div className="space-y-1.5 text-sm">
+                        <div className="flex">
+                          <span className="flex-1">Shipping</span>
+                          <span>Free</span>
+                        </div>
+                        <div className="flex">
+                          <span className="flex-1">Transaction Fee</span>
+                          <span>1</span>
+                        </div>
+                      </div>
+                    </div>
+                  </>
+                ) : (
+                  <div></div>
+                )}
             </SheetContent>
         </Sheet>
     );
--- src/components/NavItem.tsx
+++ src/components/NavItem.tsx
@@ -1,5 +1,5 @@
 import { Button } from "@/components/ui/button";
-import { cn } from "@/lib/utils";
+import { cn } from "@/src/lib/utils";
 import { ChevronDown } from "lucide-react";
 import Image from "next/image";
 import Link from "next/link";
