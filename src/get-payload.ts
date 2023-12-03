35c1e97524f108661b2a15df999d2e14a28c5e4d
Taaha Khalifa <taahakhalifa@outlook.com>
2023-12-03 18:48:40 +0000

    when a user creates an account, a verification email gets sent to their email. However, they are not yet able to verify the email

--- src/get-payload.ts
+++ src/get-payload.ts
@@ -3,11 +3,21 @@ import path from "path";
 import type { InitOptions } from "payload/config";
 import payload from "payload";
 import {Payload} from "payload";
-
+import nodemailer from "nodemailer"
 
 dotenv.config({
     path: path.resolve(__dirname, "./.env"),
 });
+
+const transporter = nodemailer.createTransport({
+    host: "smtp.resend.com",
+    secure: true,
+    port: 465,
+    auth: {
+        user: "resend",
+        pass: process.env.RESEND_API_KEY
+    }
+})
 
 let cached = (global as any).payload;
 
@@ -33,6 +43,11 @@ export const getPayLoadClient = async ({ initOptions }: Args = {}): Promise<Payl
 
     if (!cached.promise) {
         cached.promise = payload.init({
+            email: {
+                transport: transporter,
+                fromAddress: "onboarding@resend.dev",
+                fromName: "TikGadgets"
+            },
             secret: process.env.PAYLOAD_SECRET,
             local: initOptions?.express ? false : true,
             ...(initOptions || {}),
