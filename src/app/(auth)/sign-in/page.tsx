97b04c2417f8ba37bcb4d13ff081e3f19ccb8dc6
Taaha Khalifa <taahakhalifa@outlook.com>
2023-12-05 17:58:53 +0000

    when a user signs in successfully, a success mesage displays using the toast notification. also, a full reload of the current page triggers

--- src/app/(auth)/sign-in/page.tsx
+++ src/app/(auth)/sign-in/page.tsx
@@ -28,24 +28,14 @@ const Page = () => {
     });
 
     const router = useRouter()
+    const searchParams = useSearchParams()
+    const origin = searchParams.get("origin")
 
-    const { mutate, isLoading } = trpc.auth.createPayloadUser.useMutation({
-        onError: (err) => {
-            if (err.data?.code === "CONFLICT") {
-                toast.error("This email is already in use. Sign in instead?");
-                return
-            }
+    const { mutate, isLoading } = trpc.auth.signIn.useMutation({
+        onSuccess: () => {
+          toast.success("Signed in successfully")
 
-            if (err instanceof ZodError) {
-                toast.error(err.issues[0].message)
-                return
-            }
-            toast.error("Something went wrong. Please try again.")
-        },
-
-        onSuccess: ({sentToEmail}) => {
-            toast.success(`Verification email sent to ${sentToEmail}`)
-            router.push(`/verify-email?to=${sentToEmail}`)
+          router.refresh()
         }
     });
 
