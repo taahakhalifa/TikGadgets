3b190bb7d20740df216ebe7a10e38a88b5bf0d74
Taaha Khalifa <taahakhalifa@outlook.com>
2023-12-06 12:20:57 +0000

    set up a collection configuration for managing products in the application, specifying various fields and their properties, access rules, and options for product statuses. SOME FILES HAVE NOT BEEN CREATED YET WHICH WILL BE COMPLETED TO COMPLIMENT THIS AND MAKE IT WORK CORRECTLY

--- src/collections/Products/Products.ts
+++ src/collections/Products/Products.ts
@@ -1,3 +1,4 @@
+import { PRODUCT_CATEGORIES } from "../../config/index";
 import { CollectionConfig } from "payload/types";
 
 export const Products: CollectionConfig = {
@@ -22,6 +23,114 @@ export const Products: CollectionConfig = {
             label: "name",
             type: "text",
             required: true,
+        },
+        {
+            name: "description",
+            label: "Product Description",
+            type: "textarea",
+        },
+        {
+            name: "price",
+            label: "Price (GBP)",
+            type: "number",
+            min: 0,
+            max: 1000,
+            required: true,
+        },
+        {
+            name: "category",
+            label: "Product Category",
+            type: "select",
+            options: PRODUCT_CATEGORIES.map(({ label, value }) => ({
+                label,
+                value,
+            })),
+            required: true,
+        },
+        {
+            name: "product_files",
+            label: "Product File(s)",
+            type: "relationship",
+            required: true,
+            relationTo: "product_files",
+            hasMany: false,
+        },
+        {
+            name: "product_status",
+            label: "Product Status",
+            type: "select",
+            defaultValue: "pending",
+            access: {
+                create: ({ req }) => req.user.role === "admin",
+                read: ({ req }) => req.user.role === "admin",
+                update: ({ req }) => req.user.role === "admin",
+            },
+            options: [
+                {
+                    label: "Pending Verification",
+                    value: "pending",
+                },
+                {
+                    label: "Approved",
+                    value: "approved",
+                },
+                {
+                    label: "In Transit",
+                    value: "in_transit",
+                },
+                {
+                    label: "Delivered",
+                    value: "delivered",
+                },
+                {
+                    label: "Cancelled",
+                    value: "cancelled",
+                },
+            ],
+        },
+        {
+            name: "priceId",
+            type: "text",
+            access: {
+                create: () => false,
+                read: () => false,
+                update: () => false,
+            },
+            admin: {
+                hidden: true,
+            },
+        },
+        {
+            name: "stripeId",
+            type: "text",
+            access: {
+                create: () => false,
+                read: () => false,
+                update: () => false,
+            },
+            admin: {
+                hidden: true,
+            },
+        },
+        {
+            name: "images",
+            type: "array",
+            label: "Product Images",
+            minRows: 1,
+            maxRows: 5,
+            required: true,
+            labels: {
+                singular: "Image",
+                plural: "Images",
+            },
+            fields: [
+                {
+                    name: "image",
+                    type: "upload",
+                    relationTo: "media",
+                    required: true,
+                },
+            ],
         },
     ],
 };
