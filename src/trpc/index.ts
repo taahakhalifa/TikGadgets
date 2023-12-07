2d83d1cf0e27bff40baaf073ad5bd6ed96592d3a
Taaha Khalifa <taahakhalifa@outlook.com>
2023-12-07 13:50:41 +0000

    created a API route procedure for fetching products in a paginated and filtered manner. allows users to request a specific number of products per page, navigate through different pages of products using the cursor, and filter products based on various criteria using the query

--- src/trpc/index.ts
+++ src/trpc/index.ts
@@ -1,9 +1,58 @@
-import { authRouter } from "./auth-router"
-import { publicProcedure, router } from "./trpc"
-
+import { authRouter } from "./auth-router";
+import { publicProcedure, router } from "./trpc";
+import { z } from "zod";
+import { QueryValidator } from "../lib/validators/query-validators";
+import { getPayLoadClient } from "../get-payload";
 
 export const appRouter = router({
-  auth: authRouter
-})
+    auth: authRouter,
 
-export type AppRouter = typeof appRouter
+    getInfiniteProducts: publicProcedure
+        .input(
+            z.object({
+                limit: z.number().min(1).max(100),
+                cursor: z.number().nullish(),
+                query: QueryValidator,
+            })
+        )
+        .query(async ({ input }) => {
+          console.log(input);
+          
+            const { query, cursor } = input;
+            const { sort, limit, ...queryOpts } = query;
+
+            const payload = await getPayLoadClient();
+
+            const parsedQueryOpts: Record<string, { equals: string }> = {};
+
+            Object.entries(queryOpts).forEach(([key, value]) => {
+                parsedQueryOpts[key] = {
+                    equals: value,
+                };
+            });
+
+            const page = cursor || 1;
+
+            const {
+                docs: items,
+                hasNextPage,
+                nextPage,
+            } = await payload.find({
+                collection: "products",
+                where: {
+                    ...parsedQueryOpts,
+                },
+                sort,
+                limit,
+                depth: 1,
+                page,
+            });
+
+            return {
+                items,
+                nextPage: hasNextPage ? nextPage : null,
+            };
+        }),
+});
+
+export type AppRouter = typeof appRouter;
