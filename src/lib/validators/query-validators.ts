4ff5aa8bed8403520e0777781bd1582444c61246
Taaha Khalifa <taahakhalifa@outlook.com>
2023-12-07 13:59:08 +0000

    fixed type error on Desc to desc

--- src/lib/validators/query-validators.ts
+++ src/lib/validators/query-validators.ts
@@ -2,7 +2,7 @@ import { z } from "zod";
 
 export const QueryValidator = z.object({
     category: z.string().optional(),
-    sort: z.enum(["asc", "Desc"]).optional(),
+    sort: z.enum(["asc", "desc"]).optional(),
     limit: z.number().optional(),
 });
 
