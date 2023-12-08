2a6ea10d8480140e342de3a93335aedccec5565a
Taaha Khalifa <taahakhalifa@outlook.com>
2023-12-08 17:26:11 +0000

    render the productreel component onto the main home page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -46,7 +46,7 @@ export default function Home() {
                         </Link>
                     </div>
                 </div>
-                <ProductReel title="Brand new" href="/products"/>
+                <ProductReel query={{sort: "desc", limit: 4}} title="Brand new" href="/products"/>
             </MaxWidthWrapper>
 
             <section className="border-t border-gray-200 bg-gray-50">
