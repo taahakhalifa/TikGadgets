c49aef688136588406ed68c4135399dde095a1ed
Taaha Khalifa <taahakhalifa@outlook.com>
2023-12-11 16:51:59 +0000

    created a timeout function so that, when a user adds to cart, for 2 seconds, the button says 'Added!' then returns back to 'Add to cart' so that the user knows that their product has been added to the cart

--- src/components/AddToCartButton.tsx
+++ src/components/AddToCartButton.tsx
@@ -1,9 +1,30 @@
-import { Button } from "./ui/button"
+"use client";
+
+import { useEffect, useState } from "react";
+import { Button } from "./ui/button";
 
 const AddToCartButton = () => {
-return (
-  <Button size="lg" className="w-full">Add to cart</Button>
-)
-}
+    const [isSuccess, setIsSuccess] = useState<boolean>(false);
 
-export default AddToCartButton
+    useEffect(() => {
+        const timeout = setTimeout(() => {
+            setIsSuccess(false);
+        }, 2000);
+
+        return () => clearTimeout(timeout);
+    }, [isSuccess]);
+
+    return (
+        <Button
+            onClick={() => {
+                setIsSuccess(true);
+            }}
+            size="lg"
+            className="w-full"
+        >
+            {isSuccess ? "Added!" : "Add to Cart!"}
+        </Button>
+    );
+};
+
+export default AddToCartButton;
