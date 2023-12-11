71b5e1cbc6b158296b9bdaf75f661fd07a5a1381
Taaha Khalifa <taahakhalifa@outlook.com>
2023-12-11 22:13:28 +0000

    when there are no items in the cart, then a box will show in the carts page saying that the cart is empty and there is nothing to show

--- src/app/cart/page.tsx
+++ src/app/cart/page.tsx
@@ -2,9 +2,18 @@
 
 import { useCart } from "@/hooks/use-cart";
 import { cn } from "@/lib/utils";
+import Image from "next/image";
+import { useEffect, useState } from "react";
 
 const Page = () => {
     const { items, removeItem } = useCart();
+
+    const [isMounted, setIsMounted] = useState<boolean>();
+
+    useEffect(() => {
+        setIsMounted(true);
+    }, []);
+
     return (
         <div className="bg-white">
             <div className="mx-auto max-w-2xl px-4 pb-24 pt-16 sm:px-6 lg:max-w-7xl lg:px-8">
@@ -13,12 +22,31 @@ const Page = () => {
                 </h1>
                 <div className="mt-12 lg:grid lg:grid-cols-12 lg:items-start lg:gap-x-12 xl:gap-x-16">
                     <div
-                        className={cn("lg:span-7", {
+                        className={cn("lg:col-span-7", {
                             "rounded-lg border-2 border-dashed border-zinc-200 p-12":
-                                items.length === 0,
+                                isMounted && items.length === 0,
                         })}
                     >
                         <h2 className="sr-only">Items in your shopping cart</h2>
+                        {isMounted && items.length === 0 ? (
+                            <div className="flex h-full flex-col items-center justify-center space-y-1">
+                                <div
+                                    aria-hidden="true"
+                                    className="relative mb-4 h-40 w-40 text-muted-foreground"
+                                >
+                                    <Image
+                                        src="/hippo-empty-cart.png"
+                                        fill
+                                        loading="eager"
+                                        alt="empty shopping cart hippo"
+                                    />
+                                </div>
+                                <h3 className="font-semibold text-2xl">Your cart is empty</h3>
+                                <p className="text-muted-foreground text-center">
+                                  Whoops! Norhting to show here yet
+                                </p>
+                            </div>
+                        ) : null}
                     </div>
                 </div>
             </div>
