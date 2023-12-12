972531faf92b6c926f01909ac99cfe48050858d7
Taaha Khalifa <taahakhalifa@outlook.com>
2023-12-12 22:55:31 +0000

    started off creating the checkout session and what is to occur upon clicking checkout when a user has items in the cart. Firstly, I filtered the products which we obtained prior to totally make sure that the products we want to add MOST CERTAINLY have a priceId. Thereon after, created  a new order collection in the Payload database  with the list of product IDs and the user's ID, marking it as unpaid. Finally, upon a successful url, the user will get redirected to a page where its url contains the orderID

--- src/trpc/payment-router.ts
+++ src/trpc/payment-router.ts
@@ -2,6 +2,7 @@ import { router, privateProcedure } from "./trpc";
 import { z } from "zod";
 import { TRPCError } from "@trpc/server";
 import { getPayLoadClient } from "../get-payload";
+import { stripe } from "../lib/stripe";
 
 export const paymentRouter = router({
     createSession: privateProcedure
@@ -24,5 +25,24 @@ export const paymentRouter = router({
                     },
                 },
             });
+
+            const filteredProducts = products.filter((product) =>
+                Boolean(product.priceId)
+            );
+
+            const order = await payload.create({
+                collection: "orders",
+                data: {
+                    _isPaid: false,
+                    products: filteredProducts.map((prod) => prod.id),
+                    user: user.id,
+                },
+            });
+
+            try {
+                const stripeSession = await stripe.checkout.sessions.create({
+                    success_url: `${process.env.NEXT_PUBLIC_SERVER_URL}/thank-you?orderId=${order.id}`,
+                });
+            } catch (err) {}
         }),
 });
