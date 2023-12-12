fb38d1e0fb16c3509f404debc7b3c52b2f5bacb2
Taaha Khalifa <taahakhalifa@outlook.com>
2023-12-12 22:59:17 +0000

    for when a user is to pay, I have added in the mode of payment and payment types, along with metadata to know which user checked out and also which order the user payed for once they pay

--- src/trpc/payment-router.ts
+++ src/trpc/payment-router.ts
@@ -43,6 +43,12 @@ export const paymentRouter = router({
                 const stripeSession = await stripe.checkout.sessions.create({
                     success_url: `${process.env.NEXT_PUBLIC_SERVER_URL}/thank-you?orderId=${order.id}`,
                     cancel_url: `${process.env.NEXT_PUBLIC_SERVER_URL}/cart`,
+                    payment_method_types: ["card", "paypal"],
+                    mode: "payment",
+                    metadata: {
+                        userId: user.id,
+                        orderId: order.id,
+                    },
                 });
             } catch (err) {}
         }),
