2db86aa9b198db18ac19dd7bbd4911787a5d30dc
Taaha Khalifa <taahakhalifa@outlook.com>
2023-12-13 20:03:28 +0000

    now get the user from the server side in this component

--- src/app/thank-you/page.tsx
+++ src/app/thank-you/page.tsx
@@ -1,21 +1,41 @@
-import Image from "next/image"
+import { getServerSideUser } from "@/lib/payload-utils";
+import Image from "next/image";
+import { cookies } from "next/headers";
 
-const ThankYouPage = () => {
-return (
-  <main className="relative lg:min-h-full">
-    <div className="hidden md:block h-80 overlow-hidden lg:absolute lg:h-full lg:w-1/2 lg:pr-4 xl:pr-12">
-      <Image fill src="/checkout-thank-you.jpg" className="h-full w-full object-cover object-center" alt="thank you for your order"/>
-    </div>
-    <div>
-      <div className="mx-auto max-w-2xl px-4 py-16 sm:px-6 sm:py-24 lg:grid lg:max-w-7xl lg:grid-cols-2 lg:gap-x-8 lg:px-8 lg:py-32 xl:gap-x-24">
-        <div className="lg:col-start-2">
-          <p className="text-sm font-medium text-blue-600">Order successful</p>
-          <h1 className="mt-2 text-4xl font-bold tracking-tight text-gray-900 sm:text-5xl">Thanks for ordering</h1>
-        </div>
-      </div>
-    </div>
-  </main> 
-)
+interface PageProps {
+    searchParams: { [key: string]: string | string[] | undefined };
 }
 
-export default ThankYouPage
+const ThankYouPage = async ({ searchParams }: PageProps) => {
+    const orderId = searchParams.orderId;
+    const nextCookies = cookies();
+
+    const { user } = await getServerSideUser(nextCookies);
+
+    return (
+        <main className="relative lg:min-h-full">
+            <div className="hidden lg:block h-80 overlow-hidden lg:absolute lg:h-full lg:w-1/2 lg:pr-4 xl:pr-12">
+                <Image
+                    fill
+                    src="/checkout-thank-you.jpg"
+                    className="h-full w-full object-cover object-center"
+                    alt="thank you for your order"
+                />
+            </div>
+            <div>
+                <div className="mx-auto max-w-2xl px-4 py-16 sm:px-6 sm:py-24 lg:grid lg:max-w-7xl lg:grid-cols-2 lg:gap-x-8 lg:px-8 lg:py-32 xl:gap-x-24">
+                    <div className="lg:col-start-2">
+                        <p className="text-sm font-medium text-blue-600">
+                            Order successful
+                        </p>
+                        <h1 className="mt-2 text-4xl font-bold tracking-tight text-gray-900 sm:text-5xl">
+                            Thanks for ordering
+                        </h1>
+                    </div>
+                </div>
+            </div>
+        </main>
+    );
+};
+
+export default ThankYouPage;
