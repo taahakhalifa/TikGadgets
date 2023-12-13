bd90b7d500f862961769a5dd0912102282b5dd77
Taaha Khalifa <taahakhalifa@outlook.com>
2023-12-13 20:20:50 +0000

    now rendering on the thank you page is the basic information of the actual order such as where the receipt and details are sent to, and the order number

--- src/app/thank-you/page.tsx
+++ src/app/thank-you/page.tsx
@@ -55,6 +55,25 @@ const ThankYouPage = async ({ searchParams }: PageProps) => {
                         <h1 className="mt-2 text-4xl font-bold tracking-tight text-gray-900 sm:text-5xl">
                             Thanks for ordering
                         </h1>
+                        {order._isPaid ? (
+                            <p className="mt-2 text-base text-muted-foreground">
+                                Your order was processed. We&apos;ve sent your
+                                receipt and order details to{" "}
+                                {typeof order.user !== "string" ? (
+                                    <span className="font-medium text-gray-900">
+                                        {order.user.email}
+                                    </span>
+                                ) : null}
+                                .
+                            </p>
+                        ) : (
+                            <p className="mt-2 text-base text-muted-foreground">We appreciate your order, and we&apos;re currently processing it. So hang tight and we&apos;ll send you confirmation very soon!</p>
+                        )}
+
+                        <div className="mt-16 text-sm font-medium">
+                          <div className="text-muted-foreground">Order number:</div>
+                          <div className="mt-2 text-gray-900">{order.id}</div>
+                        </div>
                     </div>
                 </div>
             </div>
