6d106b7b1e7725e84ca99bf847dcdb8757132af2
Taaha Khalifa <taahakhalifa@outlook.com>
2023-12-14 19:06:15 +0000

    We validate the event by using Stripe's constructEvent method. The method takes the raw body, the signature, and Stripe webhook secret to validate the request. If the signature does not match the expected signature, constructEvent will throw an error 400 with an error message. This ensures that only requests with a valid signature are processed.

--- src/webhooks.ts
+++ src/webhooks.ts
@@ -1,5 +1,7 @@
 import express from "express";
 import { WebhookRequest } from "./server";
+import { stripe } from "./lib/stripe";
+import type Stripe from "stripe";
 
 export const stripeWebhookHandler = async (
     req: express.Request,
@@ -8,4 +10,21 @@ export const stripeWebhookHandler = async (
     const webhookRequest = req as any as WebhookRequest;
     const body = webhookRequest.rawBody;
     const signature = req.headers["stripe-signature"] || "";
+
+    let event;
+    try {
+        event = stripe.webhooks.constructEvent(
+            body,
+            signature,
+            process.env.STRIPE_WEBHOOK_SECRET || ""
+        );
+    } catch (err) {
+        return res
+            .status(400)
+            .send(
+                `Webhook Error: ${
+                    err instanceof Error ? err.message : "Unknown Error"
+                }`
+            );
+    }
 };
