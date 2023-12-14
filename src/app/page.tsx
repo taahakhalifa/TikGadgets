86fa632e9b6d066f3fc5a5d2732fb80401492785
Taaha Khalifa <taahakhalifa@outlook.com>
2023-12-14 21:01:15 +0000

    changed the code for when I wrote ' as literal apostrophies as opposed to &apos;

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -31,14 +31,14 @@ export default function Home() {
             <MaxWidthWrapper>
                 <div className="py-20 mx-auto text-center flex flex-col items-center max-w-3xl">
                     <h1 className="text-4xl font-bold tracking-tight">
-                        Uncover TikTok's Hidden Gems: Must-Have{" "}
+                        Uncover TikTok&apos;s Hidden Gems: Must-Have{" "}
                         <span className="text-blue-600">Products</span> and{" "}
                         <span className="text-blue-600">Gadgets</span>.
                     </h1>
                     <p className="mt-6 text-lg max-w-prose text-muted-foreground">
-                        Welcome to the world of TikTok's hidden gems, where
-                        you'll discover must-have products and gadgets that
-                        bring joy to your life. Let's explore together!
+                        Welcome to the world of TikTok&apos;s hidden gems, where
+                        you&apos;ll discover must-have products and gadgets that
+                        bring joy to your life. Let&apos;s explore together!
                     </p>
                     <div className="flex flex-col sm:flex-row gap-4 mt-6">
                         <Link href="/products" className={buttonVariants()}>
