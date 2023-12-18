e0ccb666cca973533761f0cc5dacb5074359e917
Taaha Khalifa <taahakhalifa@outlook.com>
2023-12-18 18:29:54 +0000

    only admins and the user which is logged in that matches the user.id can read this particluar User. likewise, anyone can create a User. However, ONLY THE ADMIN can update or delete a user

--- src/collections/Users.ts
+++ src/collections/Users.ts
@@ -1,15 +1,51 @@
-import { CollectionConfig } from "payload/types";
+import { CollectionConfig, Access } from "payload/types";
+
+const adminsAndUser: Access = ({req: {user}}) => {
+    if (user.role === "admin") return true
+
+    return {
+        id: {
+            equals: user.id
+        }
+    }
+}
 
 export const Users: CollectionConfig = {
     slug: "users",
     auth: {
         verify: {
-            generateEmailHTML: ({token}) => {
-                return `<a href="${process.env.NEXT_PUBLIC_SERVER_URL}/verify-email?token=${token}">Verify account</a>`
-            }
-        }
+            generateEmailHTML: ({ token }) => {
+                return `<a href="${process.env.NEXT_PUBLIC_SERVER_URL}/verify-email?token=${token}">Verify account</a>`;
+            },
+        },
+    },
+    access: {
+        read: adminsAndUser,
+        create: () => true,
+        update: ({req}) => req.user.role === "admin",
+        delete: ({req}) => req.user.role === "admin"
     },
     fields: [
+        {
+            name: "products",
+            label: "Products",
+            admin: {
+                condition: () => true,
+            },
+            type: "relationship",
+            relationTo: "products",
+            hasMany: true,
+        },
+        {
+            name: "product_files",
+            label: "Product Files",
+            admin: {
+                condition: () => true,
+            },
+            type: "relationship",
+            relationTo: "product_files",
+            hasMany: true,
+        },
         {
             name: "role",
             defaultValue: "user",
