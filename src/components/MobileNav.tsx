a539a32bb71aafbbd0f1bbccd5d3e81bc33daa94
Taaha Khalifa <taahakhalifa@outlook.com>
2023-12-26 18:42:27 +0000

    displaying product categories in the mobile view navbar for the user to go to whichever category they desire

--- src/components/MobileNav.tsx
+++ src/components/MobileNav.tsx
@@ -2,6 +2,9 @@
 
 import { Menu, X } from "lucide-react";
 import { useState } from "react";
+import { PRODUCT_CATEGORIES } from "../config";
+import Image from "next/image";
+import Link from "next/link";
 
 const MobileNav = () => {
     const [isOpen, setIsOpen] = useState<boolean>(false);
@@ -36,6 +39,49 @@ const MobileNav = () => {
                             >
                                 <X className="h-6 w-6" aria-hidden="true" />
                             </button>
+                        </div>
+
+                        <div className="mt-2">
+                            <ul>
+                                {PRODUCT_CATEGORIES.map((category) => (
+                                    <li
+                                        key={category.label}
+                                        className="space-y-10 px-4 pb-8 pt-10"
+                                    >
+                                        <div className="border-b border-gray-200">
+                                            <div className="-mb-px flex">
+                                                <p className="border-transparent text-gray-900 flex-1 whitespace-nowrap border-b-2 py-4 text-base font-semibold">
+                                                    {category.label}
+                                                </p>
+                                            </div>
+                                        </div>
+
+                                        <div className="grid grid-cols-2 gap-y-10 gap-x-4">
+                                            {category.feature.map((item) => (
+                                                <div
+                                                    key={item.name}
+                                                    className="group relative text-sm"
+                                                >
+                                                    <div className="relative aspect-square overflow-hidden rounded-lg bg-gray-100 group-hover:opacity-75">
+                                                        <Image
+                                                            fill
+                                                            src={item.imageSrc}
+                                                            alt="product category image"
+                                                            className="object-cover object-center"
+                                                        />
+                                                    </div>
+                                                    <Link
+                                                        href={item.href}
+                                                        className="mt-6 block font-medium text-gray-900"
+                                                    >
+                                                        {item.name}
+                                                    </Link>
+                                                </div>
+                                            ))}
+                                        </div>
+                                    </li>
+                                ))}
+                            </ul>
                         </div>
                     </div>
                 </div>
