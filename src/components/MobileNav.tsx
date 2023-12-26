16c27857543004e36ad87fbc6b87b5526366b72c
Taaha Khalifa <taahakhalifa@outlook.com>
2023-12-26 18:37:23 +0000

    when a user clicks on the mobile nav button, the button will disappear from the navbar and a navigation scroller will appear on the left. It will make the entire screen go sort of grey so that the navigation scroller on the left is clearly visible. In the navigation scroller, the only thing there is is an X button for the user to click when they want to exit from the scroller on the left. Other than that, it is just white space

--- src/components/MobileNav.tsx
+++ src/components/MobileNav.tsx
@@ -1,15 +1,47 @@
-import { Menu } from 'lucide-react'
+"use client";
+
+import { Menu, X } from "lucide-react";
+import { useState } from "react";
 
 const MobileNav = () => {
-  return (
-    <div>
-      <button
-        type='button'
-        className='lg:hidden relative -m-2 inline-flex items-center justify-center rounded-md p-2 text-gray-400 '>
-        <Menu className='h-6 w-6' aria-hidden='true' />
-      </button>
-    </div>
-  )
-}
+    const [isOpen, setIsOpen] = useState<boolean>(false);
 
-export default MobileNav
+    if (!isOpen)
+        return (
+            <div>
+                <button
+                    type="button"
+                    onClick={() => setIsOpen(true)}
+                    className="lg:hidden relative -m-2 inline-flex items-center justify-center rounded-md p-2 text-gray-400 "
+                >
+                    <Menu className="h-6 w-6" aria-hidden="true" />
+                </button>
+            </div>
+        );
+
+    return (
+        <div>
+            <div className="relative z-40 lg:hidden">
+                <div className="fixed inset-0 bg-black bg-opacity-25" />
+            </div>
+
+            <div className="fixed overflow-y-scroll overscroll-y-none inset-0 z-40 flex">
+                <div className="w-4/5">
+                    <div className="relative flex w-full max-w-sm flex-col overflow-y-auto bg-white pb-12 shadow-xl">
+                        <div className="flex px-4 pb-2 pt-5">
+                            <button
+                                type="button"
+                                onClick={() => setIsOpen(false)}
+                                className="relative -m-2 inline-flex items-center justify-center rounded-md p-2 text-gray-400"
+                            >
+                                <X className="h-6 w-6" aria-hidden="true" />
+                            </button>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    );
+};
+
+export default MobileNav;
