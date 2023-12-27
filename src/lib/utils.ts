df85c316e20689525064dd2385813ce8dcedb9b4
Taaha Khalifa <taahakhalifa@outlook.com>
2023-12-27 17:22:34 +0000

    contructed a new metadata so that the sites metadata reflects my actual site

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -1,5 +1,6 @@
 import { type ClassValue, clsx } from "clsx";
 import { twMerge } from "tailwind-merge";
+import { Metadata } from 'next'
 
 export function cn(...inputs: ClassValue[]) {
     return twMerge(clsx(inputs));
@@ -20,4 +21,40 @@ export function formatPrice(
         notation,
         maximumFractionDigits: 2,
     }).format(numbericPrice);
+}
+
+export function constructMetadata({
+    title = "TikGadgets - the marketplace for TikTok's hidden gems",
+    description = "TikGadgets is an open-source marketplace for high-quality TikTok gadgets.",
+    image = "/thumbnail.png",
+    icons = "/favicon.ico",
+    noIndex = false,
+}: {
+    title?: string;
+    description?: string;
+    image?: string;
+    icons?: string;
+    noIndex?: boolean;
+} = {}): Metadata {
+    return {
+        title,
+        description,
+        openGraph: {
+            title,
+            description,
+            images: [
+                {
+                    url: image,
+                },
+            ],
+        },
+        icons,
+        metadataBase: new URL("https://www.tikgadgets.co.uk"),
+        ...(noIndex && {
+            robots: {
+                index: false,
+                follow: false,
+            },
+        }),
+    };
 }
