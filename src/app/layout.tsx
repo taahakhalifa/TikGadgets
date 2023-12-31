ae905dfe15e0327dd5ca921903bbd708214944bd
Taaha Khalifa <taahakhalifa@outlook.com>
2023-12-31 14:53:13 +0000

    changed the comment to ts-ignore

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -25,7 +25,7 @@ export default function RootLayout({
             >
                 <main className="relative flex flex-col min-h-screen">
                     <Providers>
-                        {/* @ts-expect-error Server Component */}
+                        {/* @ts-ignore */}
                         <Navbar />
                         <div className="flex-grow flex-1">{children}</div>
                         <Footer />
