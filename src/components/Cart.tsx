b4f31e8f25121219ffae2c56496e6515f8e1241a
Taaha Khalifa <taahakhalifa@outlook.com>
2024-01-04 15:15:13 +0000

    Updated the Cart sheet so that the only thing which is scrollable is the contained within the cart and not anything else

--- src/components/Cart.tsx
+++ src/components/Cart.tsx
@@ -46,13 +46,13 @@ const Cart = () => {
                     {isMounted ? itemCount : 0}
                 </span>
             </SheetTrigger>
-            <SheetContent className="flex w-full flex-col pr-0 sm:max-2-lg">
+            <SheetContent className="flex w-full flex-col pr-0">
                 <SheetHeader className="space-y-2.5 pr-6">
                     <SheetTitle>Cart ({itemCount})</SheetTitle>
                 </SheetHeader>
-                <ScrollArea>
-                    {itemCount > 0 ? (
-                        <>
+                {itemCount > 0 ? (
+                    <>
+                        <ScrollArea>
                             <div className="flex w-full flex-col pr-6">
                                 {items.map(({ product }) => (
                                     <CartItem
@@ -60,74 +60,70 @@ const Cart = () => {
                                         product={product}
                                     />
                                 ))}
-                                Cart Item
                             </div>
-                            <div className="space-y-4 pr-6">
-                                <Separator />
-                                <div className="space-y-1.5 text-sm">
-                                    <div className="flex">
-                                        <span className="flex-1">Shipping</span>
-                                        <span>Free</span>
-                                    </div>
-                                    <div className="flex">
-                                        <span className="flex-1">
-                                            Transaction Fee
-                                        </span>
-                                        <span>{formatPrice(fee)}</span>
-                                    </div>
-                                    <div className="flex">
-                                        <span className="flex-1">Total</span>
-                                        <span>
-                                            {formatPrice(cartTotal + fee)}
-                                        </span>
-                                    </div>
+                        </ScrollArea>
+                        <div className="space-y-4 pr-6">
+                            <Separator />
+                            <div className="space-y-1.5 text-sm">
+                                <div className="flex">
+                                    <span className="flex-1">Shipping</span>
+                                    <span>Free</span>
                                 </div>
+                                <div className="flex">
+                                    <span className="flex-1">
+                                        Transaction Fee
+                                    </span>
+                                    <span>{formatPrice(fee)}</span>
+                                </div>
+                                <div className="flex">
+                                    <span className="flex-1">Total</span>
+                                    <span>{formatPrice(cartTotal + fee)}</span>
+                                </div>
+                            </div>
 
-                                <SheetFooter>
-                                    <SheetTrigger asChild>
-                                        <Link
-                                            href="/cart"
-                                            className={buttonVariants({
-                                                className: "w-full",
-                                            })}
-                                        >
-                                            Continue to Checkout
-                                        </Link>
-                                    </SheetTrigger>
-                                </SheetFooter>
-                            </div>
-                        </>
-                    ) : (
-                        <div className="flex h-full flex-col items-center justify-center space-y-1">
-                            <div
-                                className="relative mb-4 h-60 w-60 text-muted-foreground"
-                                aria-hidden="true"
-                            >
-                                <Image
-                                    src="/hippo-empty-cart.png"
-                                    fill
-                                    alt="empty shopping cart hippo"
-                                />
-                            </div>
-                            <div className="text-xl font-semibold">
-                                Your cart is empty
-                            </div>
-                            <SheetTrigger asChild>
-                                <Link
-                                    href="/products"
-                                    className={buttonVariants({
-                                        variant: "link",
-                                        size: "sm",
-                                        className:
-                                            "text-sm text-muted-foreground",
-                                    })}
-                                >
-                                    Add items to your cart to checkout
-                                </Link>
-                            </SheetTrigger>
+                            <SheetFooter>
+                                <SheetTrigger asChild>
+                                    <Link
+                                        href="/cart"
+                                        className={buttonVariants({
+                                            className: "w-full",
+                                        })}
+                                    >
+                                        Continue to Checkout
+                                    </Link>
+                                </SheetTrigger>
+                            </SheetFooter>
                         </div>
-                    )}
-                </ScrollArea>
+                    </>
+                ) : (
+                    <div className="flex h-full flex-col items-center justify-center space-y-1">
+                        <div
+                            className="relative mb-4 h-60 w-60 text-muted-foreground"
+                            aria-hidden="true"
+                        >
+                            <Image
+                                src="/hippo-empty-cart.png"
+                                fill
+                                alt="empty shopping cart hippo"
+                            />
+                        </div>
+                        <div className="text-xl font-semibold">
+                            Your cart is empty
+                        </div>
+                        <SheetTrigger asChild>
+                            <Link
+                                href="/products"
+                                className={buttonVariants({
+                                    variant: "link",
+                                    size: "sm",
+                                    className: "text-sm text-muted-foreground",
+                                })}
+                            >
+                                Add items to your cart to checkout
+                            </Link>
+                        </SheetTrigger>
+                    </div>
+                )}
             </SheetContent>
         </Sheet>
     );
